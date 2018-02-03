f839bb07241539857aaf352dc3d6bef7b9771a01
Katie Vaughan <vaughank92@gmail.com>
2018-02-03 16:44:41 -0500

    filtered List exercise

---
+++ fccserver/filtered.js
@@ -0,0 +1,36 @@
+let fs = require('fs');
+let path = require('path');
+
+//input
+let args = process.argv.slice(2);
+
+function filterDirectory(returnFunction) {
+	//readdir(pathname, callback) returns array of file names
+
+	let pathname = path.normalize(args[0]);
+	let extension = '.' + args[1];
+
+	fs.readdir(pathname, function(err, files) { 	
+		if(err) {
+			console.log(err);
+		}
+		else {
+			//could've used path.extName for a comparison
+			//var filteredList = files.filter((file, index)) => file.extName(extension)
+			// extension would not need the added '.' then
+			var filteredList = files.filter((file, index) => file.includes(extension));
+		}
+
+		returnFunction(filteredList);
+
+	;})
+}
+
+function consoleLog(filteredList) {
+
+	for(var i=0; i < filteredList.length; i++) {
+		console.log(filteredList[i]);
+	}
+}
+
+filterDirectory(consoleLog);
